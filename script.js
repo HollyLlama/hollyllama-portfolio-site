dfcf76ce8fc90ae73b6b8dd3b6f05df758b85886
Holly Llama <hollyelaineyanez@outlook.com>
2020-11-21 14:19:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+jQuery(document).ready(function(){
+    jQuery('#hideshowillustration').on('click', function(event) {
+        jQuery('#illustrationsDiv').toggle();
+    });
+  });
+jQuery(document).ready(function(){
+    jQuery('#hideshowweb').on('click', function(event) {
+        jQuery('#websitesDiv').toggle('show');
+    });
+  });
+
+jQuery(document).ready(function(){
+    jQuery('#hideshowiphone').on('click', function(event) {
+        jQuery('#iphoneAppDiv').toggle('show');
+      });
+    });
+
+  jQuery(document).ready(function(){
+      jQuery('#hideshowlogo').on('click', function(event) {
+          jQuery('#logosDiv').toggle('show');
+        });
+      });
+
+    jQuery(document).ready(function(){
+        jQuery('#hideshowprint').on('click', function(event) {
+            jQuery('#printDiv').toggle('show');
+          });
+});
